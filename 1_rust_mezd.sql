136bd6cbeaf9d0cdbeb56597b4f3283aab2b7729
Michal Jelinek <jelmic@gmail.com>
2024-08-21 20:04:50 +0200

    Zpracovana 1. otazka - rust mezd

---
+++ 1_rust_mezd.sql
@@ -0,0 +1,21 @@
+# 1. Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
+SELECT concat(mzdy.v_year, ' - ', mzdy.next_year) AS period 
+	,cpib.name
+	,mzdy.value AS payroll_start
+	,mzdy.value_next_year AS payroll_end
+	,mzdy.value_next_year - mzdy.value AS payroll_difference
+	,round((mzdy.value_next_year - mzdy.value)/mzdy.value*100,2) AS difference_percent
+FROM (
+	SELECT value
+		,industry_and_region_code
+		,v_year
+		,LEAD(value,1) OVER (ORDER BY industry_and_region_code,v_year) AS value_next_year
+		,LEAD(v_year,1) OVER (ORDER BY industry_and_region_code,v_year) AS next_year		
+	FROM t_michal_jelinek_project_sql_primary_final
+	WHERE value_type_code = 5958
+) mzdy
+LEFT JOIN czechia_payroll_industry_branch cpib 
+	ON cpib.code = mzdy.industry_and_region_code
+WHERE (mzdy.next_year - mzdy.v_year) = 1
+	AND mzdy.value_next_year - mzdy.value < 0
+ORDER BY mzdy.v_year, mzdy.industry_and_region_code 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Engeto_SQL_project
 Projekt do Engeto kurzu
+## Vytvoření tabulky project_SQL_primary_final
+Spojení vybraných sloupců z tabulek czechia_payroll a czechia_price, 
+zprůměrování hodnot na jednotlivé roky
+
+Vytvoření 1_rust_mezd.sql - pro zodpovezeni první výzkumné otázky
